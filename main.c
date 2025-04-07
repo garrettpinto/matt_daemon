166efa95c3b0d42fa75916978e95793e3d997665
Garrett Pinto <93928901+garrettpinto@users.noreply.github.com>
2025-04-07 13:22:16 -0400

    Update main.c

--- main.c
+++ main.c
@@ -26,71 +26,71 @@
 // void print_usage()
 
 void cleanup() {
-	// Cleanup: remove lock file before exit
-	unlink(LOCK_FILE);
+        // Cleanup: remove lock file before exit
+        unlink(LOCK_FILE);
 }
 
 int main(void) {
-	// Check if running as "root" user
-	if (getuid() != 0) {
-		printf("This program must be ran as 'root'\n");
-		return 1;
-	}
+        // Check if running as "root" user
+        if (getuid() != 0) {
+                printf("This program must be ran as 'root'\n");
+                return 1;
+        }
 
-	// create a "matt_daemon.lock" file in /var/lock
-	// only one daemon should run at a time, if attempting to run a second, an error message indicating a creation/file opening on matt_daemon.lock attempt must po
-	// when the daemon shuts down, the matt_daemon.lock file must be deleted
+        // create a "matt_daemon.lock" file in /var/lock
+        // only one daemon should run at a time, if attempting to run a second, an error message indicating a creation/file opening on matt_daemon.lock attempt must po
+        // when the daemon shuts down, the matt_daemon.lock file must be deleted
 
-	// if lockfile exists, exit since script is already running, if not; create it and continue
-	// open(O_CREAT | O_EXCL) used instead of flock() since flock() is used to coordinate multiple processes accessing the same file, open() creates a system-wide lock that persists
-	
-	struct stat st;
-	if (stat(LOCK_DIRECTORY, &st) == -1) {
-		if (errno == ENOENT) {
-			printf("Directory doesn't exist.\n");
-			if (mkdir(LOCK_DIRECTORY, 775) == -1) {
-				perror("Error creating directory");
-				return EXIT_FAILURE;
-			}
-		}
-		else {
-			perror("Error checking directory existence.\n");
-			return EXIT_FAILURE;
-		}
-	}
+        // if lockfile exists, exit since script is already running, if not; create it and continue
+        // open(O_CREAT | O_EXCL) used instead of flock() since flock() is used to coordinate multiple processes accessing the same file, open() creates a system-wide lock that persists
 
-	int fd = open(LOCK_FILE, O_CREAT | O_EXCL, 666);
+        struct stat st;
+        if (stat(LOCK_DIRECTORY, &st) == -1) {
+                if (errno == ENOENT) {
+                        printf("Directory doesn't exist.\n");
+                        if (mkdir(LOCK_DIRECTORY, 775) == -1) {
+                                perror("Error creating directory");
+                                return EXIT_FAILURE;
+                        }
+                }
+                else {
+                        perror("Error checking directory existence.\n");
+                        return EXIT_FAILURE;
+                }
+        }
 
-	if (fd == -1) {
-		if (errno == EEXIST) {
-			fprintf(stderr, "Another instance is already running.\n");
-		}
-		else {
-			perror("Error creating lock file...");
-		}
-		return EXIT_FAILURE;
-	}
-	
-	printf("Lock file '%s' created.\n", LOCK_FILE);
+        int fd = open(LOCK_FILE, O_CREAT | O_EXCL, 666);
 
-	atexit(cleanup);
+        if (fd == -1) {
+                if (errno == EEXIST) {
+                        fprintf(stderr, "Another instance is already running.\n");
+                }
+                else {
+                        perror("Error creating lock file...");
+                }
+                return EXIT_FAILURE;
+        }
+
+        printf("Lock file '%s' created.\n", LOCK_FILE);
+
+        atexit(cleanup);
 
         pid_t child_pid = fork();
         assert(child_pid != -1);
         if (child_pid > 0)
                 exit(EXIT_SUCCESS);
-        
+
         // Create new session
         setsid();
         child_pid = fork();
         assert(child_pid != -1);
         if (child_pid > 0)
                 exit(EXIT_SUCCESS);
-        
+
         while (true)
         {
                 sleep(1);
         }
-	
+
         return EXIT_SUCCESS;
 }
